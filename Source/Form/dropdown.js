f57bc1414622d2569bd8dc2904ba326dd8d3b498
Bryan J Swift <bryan@bryanjswift.com>
2009-02-07 18:41:06 -0500

    fleshing out the api

--- Source/Form/dropdown.js
+++ Source/Form/dropdown.js
@@ -3,8 +3,15 @@ if (typeof StyledForm === 'undefined') { StyledForm = {}; }
 
 StyledForm.Dropdown = new Class({
 	Implements: [Events,Options],
+	options: {
+		mouseLeaveDelay: 350
+	},
 	initialize: function(select,options) {
 		this.setOptions(options);
 		select = $(select);
-	}
+	},
+	expand: function() { },
+	collapse: function() { },
+	mouseenterDropdown: function() { $clear(this.collapseInterval); },
+	mouseleaveDropdown: function() { this.collapseInterval = this.collapse.delay(this.options.mouseLeaveDelay,this); }
 });
