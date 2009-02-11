9cf29d7904c01efa4c2b07c09d8bba1278d96485
Bryan J Swift <bryan@bryanjswift.com>
2009-02-11 00:34:27 -0500

    made the select option more responsible for itself including event management

--- Source/Form/SelectOption.js
+++ Source/Form/SelectOption.js
@@ -4,14 +4,17 @@ if (typeof StyledForm === 'undefined') { StyledForm = {}; }
 StyledForm.SelectOption = new Class({
 	Implements: [Events,Options],
 	options: {
-		optionClass: 'option',
 		optionTag: 'li',
 		selected: false,
-		selectedClass: 'selected',
 		storageName: 'optionData'
 	},
+	config: {
+		highlightedClass: 'highlighted',
+		optionClass: 'option',
+		selectedClass: 'selected'
+	},
 	element: null,
-	events: {},
+	bound: {},
 	option: null,
 	selected: false,
 	text: null,
@@ -20,37 +23,55 @@ StyledForm.SelectOption = new Class({
 		this.setOptions(options);
 		option = $(option);
 		this.option = option;
-		this.events = {
+		this.bound = {
 			deselect: this.deselect.bind(this),
+			highlight: this.highlight.bind(this),
+			removeHighlight: this.removeHighlight.bind(this),
 			select: this.select.bind(this)
 		};
 		this.text = option.get('text');
 		this.value = option.get('value');
 		this.element = new Element(this.options.optionTag,{
-			'class': (option.get('class') + ' ' + this.options.optionClass).trim(),
+			'class': (option.get('class') + ' ' + this.config.optionClass).trim(),
 			'html': option.get('html'),
 			'events': {
-				click: this.events.select
+				click: this.bound.select
+				mouseenter: this.bound.highlight,
+				mouseleave: this.bound.removeHighlight
 			}
 		});
 		this.element.store(this.options.storageName,this);
 		option.store(this.options.storageName,this);
 	},
 	deselect: function(e) {
-		var evt = e ? new Event(e).stop() : null;
-		this.fireEvent('onDeselect',this);
-		this.element.removeClass(this.options.selectedClass).addEvent('click',this.events.select);
+		this.fireEvent('onDeselect',[this,e]);
+		this.element.removeClass(this.config.selectedClass).addEvent('click',this.bound.select);
 		this.selected = false;
 	},
 	destroy: function() {
 		this.element = null;
-		this.events = null;
+		this.bound = null;
 		this.option = null;
 	},
+	disable: function() {
+		this.element.removeEvents({mouseenter:this.bound.highlght, mouseleave:this.bound.removeHighlight});
+	},
+	enable: function() {
+		this.element.addEvents({mouseenter:this.bound.highlght, mouseleave:this.bound.removeHighlight});
+	},
+	highlight: function() {
+		this.element.addClass(this.config.highlightedClass);
+		this.fireEvent('onHighlight',this);
+		return this;
+	},
+	removeHighlight: function() {
+		this.element.removeClass(this.config.highlightedClass);
+		this.fireEvent('onRemoveHighlight',this);
+		return this;
+	},
 	select: function(e) {
-		var evt = e ? new Event(e).stop() : null;
-		this.fireEvent('onSelect',this);
-		this.element.addClass(this.options.selectedClass).removeEvent('click',this.events.select);
+		this.fireEvent('onSelect',[this,e]);
+		this.element.addClass(this.config.selectedClass).removeEvent('click',this.bound.select);
 		this.selected = true;
 	}
 });
