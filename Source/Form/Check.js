7b22ec365570af05bc76131a55f1f6a38f591108
Bryan Swift <bryan.j.swift@gmail.com>
2009-02-12 15:51:30 -0500

    initial commit of StyledForm.Check class

---
+++ Source/Form/Check.js
@@ -0,0 +1,82 @@
+/*extern Class, Events, Options, StyledForm, $, Element */
+if (typeof StyledForm === 'undefined') { StyledForm = {}; }
+
+StyledForm.Check = new Class({
+	Implements: [Events,Options],
+	options: {
+		checked: false,
+		disabled: false
+	},
+	bound: {},
+	checked: false,
+	config: {
+		checkedClass: 'checked',
+		disabledClass: 'disabled',
+		highlightedClass: 'highlighted'
+	},
+	disabled: false,
+	element: null,
+	input: null,
+	label: null,
+	initialize: function(input,options) {
+		this.setOptions(options);
+		this.bound = {
+			disable: this.disable.bind(this),
+			enable: this.enable.bind(this),
+			highlight: this.highlight.bind(this),
+			removeHighlight: this.removeHighlight.bind(this),
+			toggle: this.toggle.bind(this)
+		};
+		var bound = this.bound;
+		input = this.input = $(input);
+		this.label = document.getElement('label[for=' + input.get('id'));
+		if (this.label) { this.label.addEvent('click',bound.toggle); }
+		this.element = new Element('div',{
+			'class': input.get('class') + ' check',
+			id: input.get('id') + 'Check',
+			events: {
+				click: bound.toggle,
+				mouseenter: bound.highlight,
+				mouseleave: bound.removeHighlight
+			}
+		});
+		this.element.wraps(input);
+		if (this.options.checked || input.get('checked')) { this.toggle(); }
+		if (this.options.disabled || input.get('disabled')) { this.disable(); }
+		this.fireEvent('onCreate',this);
+	},
+	disable: function() {
+		this.element.addClass(this.config.disabledClass);
+		this.input.set('disabled','disabled');
+		this.disabled = true;
+		this.fireEvent('onDisable',this);
+	},
+	enable: function() {
+		this.element.removeClass(this.config.disabledClass);
+		this.input.erase('disabled');
+		this.disabled = false;
+		this.fireEvent('onEnable',this);
+	},
+	highlight: function() {
+		this.element.addClass(this.config.highlightedClass);
+		this.fireEvent('onHighlight',this);
+	},
+	removeHighlight: function() {
+		this.element.removeClass(this.config.highlightedClass);
+		this.fireEvent('onRemoveHighlight',this);
+	},
+	toggle: function() {
+		if (this.disabled) { return; }
+		if (this.checked) {
+			this.element.removeClass(this.config.checkedClass);
+			this.input.erase('checked');
+			this.checked = false;
+		} else {
+			this.element.addClass(this.config.checkedClass);
+			this.input.set('checked','checked');
+			this.checked = true;
+		}
+		this.fireEvent(this.checked ? 'onCheck' : 'onUncheck',this);
+		this.fireEvent('onChange',this);
+	}
+});
